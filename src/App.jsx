ff50a01e2543ef2c0ddec559d6bdcd391b45b396
zinuu4 <unvm7721@gmail.com>
2023-06-20 12:34:00 +0300

    implemented layout and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='todo-app'>
-      <h1>Todo App</h1>
+      <TodoList/>
     </div>
   );
 }
