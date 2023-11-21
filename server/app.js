50d706e156424cc4b2e759c669e8243b19fe5479
zinuu4 <unvm7721@gmail.com>
2023-11-21 17:59:25 +0200

    feat: connect mongodb

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-const PORT = 5001;
+const PORT = process.env.APP_PORT || 5001;
+const DB_URL = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ll8m8hm.mongodb.net/?retryWrites=true&w=majority`;
+
 const app = express();
 
 app.get('/', (req, res, next) => {
   res.send('Server works');
 });
 
-app.listen(PORT);
+const startApp = async () => {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startApp();
