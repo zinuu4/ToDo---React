c5d880746fc51988de65c632475cbaa86f937bc9
zinuu4 <unvm7721@gmail.com>
2023-11-26 19:27:58 +0200

    fix: links href's to include current lang

--- client/src/components/Layout/Header/MainMenu/MainMenu.tsx
+++ client/src/components/Layout/Header/MainMenu/MainMenu.tsx
@@ -1,15 +1,23 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
 
 import { Pages, routes } from '@/shared/routes';
+import { useGetCurrentLocale } from '@/shared/utils/useGetCurrentLocale';
 
 import styles from './MainMenu.module.scss';
 
 export const MainMenu = () => {
+  const currentLocal = useGetCurrentLocale();
   return (
     <div>
       {Object.entries(routes).map(([key, { title, path }]) => (
-        <Link href={path} className={styles.link} key={key as Pages}>
+        <Link
+          href={`/${currentLocal}${path}`}
+          className={styles.link}
+          key={key as Pages}
+        >
           {title}
         </Link>
       ))}
--- client/src/components/Layout/Header/ProfileButton/ProfileButton.tsx
+++ client/src/components/Layout/Header/ProfileButton/ProfileButton.tsx
@@ -1,17 +1,24 @@
+'use client';
+
 import React from 'react';
 import { CgProfile } from 'react-icons/cg';
 import { CiLogin } from 'react-icons/ci';
 import Link from 'next/link';
 
 import { Pages, routes } from '@/shared/routes';
+import { useGetCurrentLocale } from '@/shared/utils/useGetCurrentLocale';
 
 import styles from './ProfileButton.module.scss';
 
 export const ProfileButton = () => {
+  const currentLocal = useGetCurrentLocale();
   const isAuthed = true;
   const desiredRoute: Pages = isAuthed ? 'profile' : 'login';
   return (
-    <Link href={routes[desiredRoute].path} className={styles.wrapper}>
+    <Link
+      href={`/${currentLocal}${routes[desiredRoute].path}`}
+      className={styles.wrapper}
+    >
       {isAuthed ? (
         <CgProfile style={{ color: 'white' }} />
       ) : (
