9062f565e01f0b273a5fef4750a5c0d707ac13b3
zinuu4 <unvm7721@gmail.com>
2023-11-26 19:11:24 +0200

    feat: add new pages

--- client/src/app/[lang]/todos/page.tsx
+++ client/src/app/[lang]/todos/page.tsx
@@ -1,9 +1,8 @@
 import { getDictionary } from '@/shared/localization/dictionaries';
 import { Locales } from '@/shared/types';
+import { TodoList } from '@/components/TodoList';
 
-import { TodoList } from '../../components/TodoList/TodoList';
-
-export default async function Home({ params }: { params: { lang: Locales } }) {
+export default async function Todos({ params }: { params: { lang: Locales } }) {
   const dictionary = await getDictionary(params.lang);
   return (
     <main>
