04a22095373b7395ffdbb7bcceebfb250e56c268
zinuu4 <unvm7721@gmail.com>
2023-11-26 19:34:47 +0200

    feat: add main menu active item functionality

--- client/src/components/Layout/Header/MainMenu/MainMenu.tsx
+++ client/src/components/Layout/Header/MainMenu/MainMenu.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import Link from 'next/link';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 
 import { Pages, routes } from '@/shared/routes';
 import { useGetCurrentLocale } from '@/shared/utils/useGetCurrentLocale';
@@ -9,18 +11,22 @@ import { useGetCurrentLocale } from '@/shared/utils/useGetCurrentLocale';
 import styles from './MainMenu.module.scss';
 
 export const MainMenu = () => {
+  const pathname = usePathname();
   const currentLocal = useGetCurrentLocale();
   return (
     <div>
-      {Object.entries(routes).map(([key, { title, path }]) => (
-        <Link
-          href={`/${currentLocal}${path}`}
-          className={styles.link}
-          key={key as Pages}
-        >
-          {title}
-        </Link>
-      ))}
+      {Object.entries(routes).map(([key, { title, path }]) => {
+        const isActive = `/${currentLocal}${path}` === pathname;
+        return (
+          <Link
+            href={`/${currentLocal}${path}`}
+            className={clsx(styles.link, isActive && styles.active)}
+            key={key as Pages}
+          >
+            {title}
+          </Link>
+        );
+      })}
     </div>
   );
 };
